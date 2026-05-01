3b3f128dadd059ad701ccd48ae99c5e4a99813f8
A-Hell <ArmaghanLodhi@gmail.com>
2026-05-01 15:48:42 +0500

    Add project files.

---
+++ Chess - Semester Project/Main.cpp
@@ -0,0 +1,16 @@
+// Pre-Built Librarires
+#include <iostream>
+//#include <SFML/Graphics.hpp>
+
+// Custom Libraries
+#include "Header.h"
+
+using namespace std;
+
+
+// Main code
+
+int main()
+{
+
+}
